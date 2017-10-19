f10b5c4a688fbb2c94d01287fbd27f8fd6f4e50c
adithipm <adithipm@gmail.com>
2017-10-18 21:40:47 -0700

    Add files via upload

---
+++ Application3.java
@@ -0,0 +1,13 @@
+
+public class Application3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Player P1 = new Player();
+		P1.play();
+		int score = P1.getScore();
+		System.out.println("Your score is: " + score);
+	}
+
+}
